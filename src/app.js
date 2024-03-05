4d25de13c3fb41a13a5afc292eaa8b7b295309e9
niravpansheriya <niravpanseriya2004@gmail.com>
2024-03-05 20:24:39 +0530

    User and Video model added

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import expresss from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = expresss()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(expresss.json({
+    limit: "16kb"
+}))
+
+app.use(expresss.urlencoded({
+    extended: true,
+    limit: "16kb"
+}))
+
+app.use(expresss.static("public"))
+
+app.use(cookieParser())
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env"
 })
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`app is running on ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log(`MongoDB Connection Failed... !! ${err}`)
+    })
